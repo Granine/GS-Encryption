154f343d550b81e0000d8a359aa744f94c45995b
Granine <guanzheng.huang@hotmail.com>
2023-04-08 23:34:21 -0700

    Added test for swap function

--- gs_encrypt_test/gs_encrypt_internal_function_test.py
+++ gs_encrypt_test/gs_encrypt_internal_function_test.py
@@ -23,3 +23,19 @@ def test_shift_data_location_left():
     assert encrypter._shift_data_location(raw_data, 1, "l").decode() == "23451"
     assert encrypter._shift_data_location(raw_data, 5, "l").decode() == "12345"
     
+def test_swap_data_location():
+    '''basic tests for _swap_data_location for shifting right
+    WIP:
+    need to test size above limit
+    need to test fail case (index out of bound)
+    need to test case where wrapping happen
+    need to test where swap location overlapped (12345)swap 0, 2, 3
+    '''
+    data = "12345"
+    raw_data = str.encode(data)
+    raw_data = bytearray(raw_data)
+    assert encrypter._swap_data_location(raw_data, 0, 0).decode() == "12345"
+    assert encrypter._swap_data_location(raw_data, 0, 3, 0).decode() == "12345"
+    assert encrypter._swap_data_location(raw_data, 0, 1).decode() == "21345"
+    assert encrypter._swap_data_location(raw_data, 0, 2, 2).decode() == "34125"
+    
